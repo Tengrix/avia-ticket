0d4f53f17f5bfc0cdd4e3b57ce51dd7e7bbd1fac
Evgeny94 <lucky10291994@gmail.com>
2023-10-04 23:06:27 +0300

    fix:bugs

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ const routes = [
     },
 
 ]
-const router = createBrowserRouter(routes,{basename:'/avia-ticket/'})
+const router = createBrowserRouter(routes, {basename:'/'})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
