081b21df62736125b0fd90ed38d0d709e365e827
Tengrix <damir861@gmail.com>
2023-10-04 21:28:23 -0400

    init commit

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ const routes = [
     },
 
 ]
-const router = createBrowserRouter(routes, {basename:'/avia'})
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
     <React.StrictMode>
