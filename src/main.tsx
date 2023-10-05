05ca29225a6ecc4828a876f6f88001087bd431e1
Tengrix <damir861@gmail.com>
2023-10-04 23:13:16 -0400

    init commit

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const routes = [
         element: <App/>,
         children: [
             {
-                path: "avia",
+                path: "/",
                 element: <Avia/>
             },
             {
